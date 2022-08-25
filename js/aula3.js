3c6cb624a944b5a519236c2ee860c7ad3080cad6
guilherme maywald <guilhermemaywald@gmail.com>
2022-08-25 09:11:37 -0300

    Atividade prática 3

---
+++ js/aula3.js
@@ -0,0 +1,12 @@
+{ // Resposta 
+    let manipulandoStrings = 'Manipulando strings com JS';
+    alert(manipulandoStrings.toUpperCase());
+
+    console.log(manipulandoStrings.slice(0,5));
+
+    alert(manipulandoStrings.length);
+
+    let concatenandoStrings = 'Vamos concatenar as duas strings';
+
+    console.log(manipulandoStrings + ' ' + concatenandoStrings);
+}
